43f2a6b91d8d09651f95bd07b0c51ea7afa9021a
Julia Zarzycka <juliaaa007@gmail.com>
2019-04-05 18:12:18 +0200

    cosmetic fix

--- 3-http-server/HTTPServer.h
+++ 3-http-server/HTTPServer.h
@@ -12,40 +12,40 @@
 
 class HTTPServer {
 public:
-	HTTPServer();
-	~HTTPServer();
+  HTTPServer();
+  ~HTTPServer();
 
-	void makeNonBlocking();
-	void listenOnAddress(int port, const char* ip);
-	void serve();
+  void makeNonBlocking();
+  void listenOnAddress(int port, const char* ip);
+  void serve();
 
 private:
-	void handleEvents();
-	void startNewConnection();
-	bool receiveMessage();
-	void reactToMessage();
-	void checkMethod();
-	bool endOfRequest() { return (isGet && query.find("\r\n\r\n") != std::string::npos) || (!isGet && contentLeft == 0); }
-	bool endOfPostHeader() { return !isGet && query.find("\r\n\r\n") != std::string::npos; }
-	void setContentInfo();
-	void sendResponse();
-	std::string getFilePath();
-	std::string formAnswer(std::string filePath);
-	std::vector<char> readFile (const char* path);
-	void printInfo();
-	void closeConnection();
+  void handleEvents();
+  void startNewConnection();
+  bool receiveMessage();
+  void reactToMessage();
+  void checkMethod();
+  bool endOfRequest() { return (isGet && query.find("\r\n\r\n") != std::string::npos) || (!isGet && contentLeft == 0); }
+  bool endOfPostHeader() { return !isGet && query.find("\r\n\r\n") != std::string::npos; }
+  void setContentInfo();
+  void sendResponse();
+  std::string getFilePath();
+  std::string formAnswer(std::string filePath);
+  std::vector<char> readFile (const char* path);
+  void printInfo();
+  void closeConnection();
 
-	sockaddr_in sin;
-	unsigned size;
-	const int sockfd;
-	std::vector<pollfd> fds;
+  sockaddr_in sin;
+  unsigned size;
+  const int sockfd;
+  std::vector<pollfd> fds;
 
-	std::string buffer;
+  std::string buffer;
   std::string query = "";
   bool isGet;
   int contentLength = 0;
   int contentLeft = 100;
-	int currentFdIndex = 0;
+  int currentFdIndex = 0;
 };
 
 #endif /*HTTPSERVER_H*/
